1408656feb0ca1b133575cd20b4d3cebecaa9adc
zephrod <valentin.le_fessant@outlouk.fr>
2025-04-25 23:29:33 +0200

    ajout nombre d'articles et tentative de filtre avancé

--- obj/Debug/net8.0/Scrapper.AssemblyInfo.cs
+++ obj/Debug/net8.0/Scrapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scrapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5237ad437b63058298b9ff5b7e99621f08f7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0ddd318c6f0d19ee0b36b820b66602bc5ac32a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scrapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scrapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
